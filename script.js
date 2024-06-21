c3a92d8ac508ba540365193e4de6e25ee7c7d982
arifhussain1313 <zakiriinguti@gmail.com>
2024-06-20 22:53:52 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var name = Arif, age = 21, gender = male;
+
+
+var legal = true, count = false, _ = false, $ = false, unlucky = hello-world;
+var 123 = false, my var = false, if = false, 123abc = false;
+var    = false;
+
+var hello = document.write("<h1>Rules for naming JS variables</h1>");
+document.write("Variable names can only contain letters, digits, underscores, and dollar signs.<br>");
+document.write("Variables must begin with a letter, $, or _.<br>");
+document.write("Variable names are case-sensitive.<br>");
+document.write("Variable names should not be JavaScript reserved words (e.g., if, true, this).");
+
+document.getElementById("demo").innerHTML = hello;
